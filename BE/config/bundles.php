7bc51234b57fcbb2385585e3584d9a40752bc244
jmpala <juanmartinpalazzolo@icloud.com>
2024-09-26 19:58:57 +0200

    86c0ep8rn
    - added paginator

--- BE/config/bundles.php
+++ BE/config/bundles.php
@@ -13,4 +13,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
---
+++ BE/src/Common/Service/PaginatorService.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Common\Service;
+
+use Doctrine\ORM\QueryBuilder;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class PaginatorService
+{
+    public function __construct(
+        private readonly PaginatorInterface $paginator
+    )
+    {
+    }
+
+    public function getApiPagination(
+        QueryBuilder $qb,
+        Request $request,
+        int $limit = 10 // TODO: move default to config
+    ): PaginationInterface {
+        $payload = $request->getPayload()
+            ->all();
+
+        $page = intval($payload['page'] ?? 1);
+        $itemsPerPage = intval($payload['itemsPerPage'] ?? $limit);
+
+        return $this->paginator
+            ->paginate(
+                $qb,
+                $page,
+                $itemsPerPage
+            );
+    }
+}
