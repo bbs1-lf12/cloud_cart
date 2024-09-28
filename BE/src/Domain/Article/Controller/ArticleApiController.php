c203e03e7933e9ac322448c1da091cc59567bb74
jmpala <juanmartinpalazzolo@icloud.com>
2024-09-28 23:12:37 +0200

    86c0ep8rn
    - refactoring controller into service
    - added endpoint to retrieve an article

--- BE/src/Domain/Article/Controller/ArticleApiController.php
+++ BE/src/Domain/Article/Controller/ArticleApiController.php
@@ -4,47 +4,33 @@ declare(strict_types=1);
 
 namespace App\Domain\Article\Controller;
 
-use App\Common\Service\PaginatorService;
-use App\Domain\Article\Service\ArticleQueryBuilderService;
+use App\Domain\Article\Service\ArticleAPIService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
-use \App\Domain\Article\Entity\Article;
 
 #[Route('/api/v1')]
 class ArticleApiController extends AbstractController
 {
     public function __construct(
         readonly EntityManagerInterface $entityManager,
-        readonly ArticleQueryBuilderService $articleQueryBuilderService,
-        readonly PaginatorService $paginator,
+        readonly ArticleAPIService $articleAPIService,
         readonly SerializerInterface $serializer
     )
     {}
 
     #[Route('/articles', methods: ['GET'])]
-    public function list(
-        Request $request
+    public function listArticles(
+        Request $request,
     ): JsonResponse
     {
-        $qb = $this->articleQueryBuilderService
-            ->selectAllArticlesQB();
-        $qb = $this->articleQueryBuilderService
-            ->addFilters(
-                $qb,
+        $page = $this->articleAPIService
+            ->listAllPage(
                 $request
             );
-
-        $page = $this->paginator
-            ->getApiPagination(
-                $qb,
-                $request
-            );
-        /** @var array<Article> $items */
-        $items = $page->getItems();
 
         return new JsonResponse(
             [
@@ -52,11 +38,31 @@ class ArticleApiController extends AbstractController
                 "totalPages" => $page->getPageCount(),
                 "articles" => $this->serializer
                     ->serialize(
-                        $items,
+                        $page->getItems(),
                         'json',
                         ['groups' => 'article:list']
                     ),
             ],
+        );
+    }
+
+    /**
+     * @throws \App\Domain\Api\Exceptions\ApiException
+     */
+    #[Route('/articles/{id}', methods: ['GET'])]
+    public function getArticle(
+        int $id
+    ): JsonResponse {
+        $article = $this->articleAPIService
+            ->getArticleById($id);
+
+        return new JsonResponse(
+            $this->serializer
+                ->serialize(
+                    $article,
+                    'json',
+                    ['groups' => 'article:list']
+                ),
         );
     }
 }
---
+++ BE/src/Domain/Article/Service/ArticleAPIService.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Article\Service;
+
+use App\Common\Service\PaginatorService;
+use App\Domain\Api\Exceptions\ApiException;
+use App\Domain\Article\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class ArticleAPIService
+{
+    public function __construct(
+        private readonly ArticleQueryBuilderService $articleQueryBuilderService,
+        private readonly PaginatorService $paginator,
+        private readonly EntityManagerInterface $entityManager,
+    )
+    {
+    }
+
+    public function listAllPage(
+        Request $request
+    ): PaginationInterface
+    {
+        $qb = $this->articleQueryBuilderService
+            ->selectAllArticlesQB();
+        $qb = $this->articleQueryBuilderService
+            ->addFilters(
+                $qb,
+                $request
+            );
+
+        return $this->paginator
+            ->getApiPagination(
+                $qb,
+                $request
+            );
+    }
+
+    /**
+     * @throws \App\Domain\Api\Exceptions\ApiException
+     */
+    public function getArticleById(
+        int $id
+    ): Article {
+        $repository = $this->entityManager
+            ->getRepository(Article::class);
+        /** @var Article $article */
+        $article = $repository
+            ->find($id);
+
+        if ($article === null) {
+            throw new ApiException(
+                'Article not found',
+                404
+            );
+        }
+
+        return $article;
+    }
+}
