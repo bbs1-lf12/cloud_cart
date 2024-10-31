6b0b2a3b2c544965d52aef7c88cdf8b4b5bac514
jmpala <juanmartinpalazzolo@icloud.com>
2024-10-31 23:33:36 +0100

    86c0jhf6a
    - added cross-origin support

--- BE/config/bundles.php
+++ BE/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
