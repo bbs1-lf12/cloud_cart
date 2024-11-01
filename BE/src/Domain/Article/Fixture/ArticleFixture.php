d4d538461522364d9c535b5cd70418d74977ac0f
jmpala <juanmartinpalazzolo@icloud.com>
2024-11-01 01:20:15 +0100

    86c0jhf6a
    - fix fixtures to relate a user to a comment

--- BE/src/Domain/Article/Fixture/ArticleFixture.php
+++ BE/src/Domain/Article/Fixture/ArticleFixture.php
@@ -7,24 +7,39 @@ namespace App\Domain\Article\Fixture;
 use App\Domain\Article\Entity\Article;
 use App\Domain\Article\Entity\Category;
 use App\Domain\Article\Entity\Comment;
+use App\Domain\User\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class ArticleFixture extends Fixture
 {
+    public const USER_QUANTITY = 10;
     public const ARTICLE_QUANTITY = 20;
     public const CATEGORY_QUANTITY = 3;
     public const COMMENT_PER_ARTICLE_QUANTITY = 20;
 
     public function load(ObjectManager $manager): void
     {
+        $users = [];
+        for ($i = 0; $i < self::USER_QUANTITY; $i++) {
+            $user = new User();
+            $user->setEmail('user' . $i . '@example.com');
+            $user->setPassword('password');
+            $user->setRoles(['ROLE_USER']);
+            $manager->persist($user);
+            $users[] = $user;
+        }
+
         for ($i = 0; $i < self::CATEGORY_QUANTITY; $i++) {
             $category = new Category();
             $category->setName('Category ' . $i);
             $category->setDescription('Description ' . $i);
             $category->setIsEnabled(true);
             $manager->persist($category);
-            $this->addReference('category_' . $i, $category);
+            $this->addReference(
+                'category_' . $i,
+                $category,
+            );
         }
 
         for ($i = 0; $i < self::ARTICLE_QUANTITY; $i++) {
@@ -35,13 +50,13 @@ class ArticleFixture extends Fixture
             $article->setStock($i * 10);
             $article->setIsFeatured($i % 2 === 0);
             $article->setIsEnabled(true);
-            $article->setScore($i * 0.1);
             $article->setCategory($this->getReference('category_' . ($i % self::CATEGORY_QUANTITY)));
 
             for ($j = 0; $j < self::COMMENT_PER_ARTICLE_QUANTITY; $j++) {
                 $comment = new Comment();
                 $comment->setContent('Comment ' . $j . ' ' . $article->getTitle());
                 $comment->setArticle($article);
+                $comment->setUser($users[$j % self::USER_QUANTITY]);
                 $manager->persist($comment);
             }
 
