d559e4718895fc984dc028d61b79e591aa7e0ffd
jmpala <juanmartinpalazzolo@icloud.com>
2024-11-27 20:03:29 +0100

    86c14tbdz
    - added support to checkboxes and selects

--- BE/assets/js/backoffice/resetFilter.js
+++ BE/assets/js/backoffice/resetFilter.js
@@ -4,8 +4,15 @@
     resetButtons.forEach((btn) => {
       btn.addEventListener('click', () => {
         const form = btn.closest('form');
-        const inputs = form.querySelectorAll('input');
-        inputs.forEach((input) => input.value = '');
+
+        const inputsText = form.querySelectorAll('input[type="text"]');
+        inputsText.forEach((input) => input.value = '');
+
+        const inputsCheckbox = form.querySelectorAll('input[type="checkbox"]');
+        inputsCheckbox.forEach((input) => input.checked = false);
+
+        const selects = form.querySelectorAll('select');
+        selects.forEach((dropdown) => dropdown.selectedIndex = 0);
       });
     })
   })
