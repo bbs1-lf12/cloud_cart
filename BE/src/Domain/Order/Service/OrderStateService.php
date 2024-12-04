7cb90f653827ad03c31946db3f65d2349a2ea22b
jmpala <juanmartinpalazzolo@icloud.com>
2024-12-04 13:05:19 +0100

    86c17e7d3
    - persist order entity on state change

--- BE/src/Domain/Order/Service/OrderStateService.php
+++ BE/src/Domain/Order/Service/OrderStateService.php
@@ -7,12 +7,14 @@ namespace App\Domain\Order\Service;
 use App\Domain\Order\Entity\Order;
 use App\Domain\Order\Exceptions\OrderStatusException;
 use App\Domain\Order\Workflow\OrderTransitions;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Workflow\WorkflowInterface;
 
 class OrderStateService
 {
     public function __construct(
         private readonly WorkflowInterface $ordersStateMachine,
+        private readonly EntityManagerInterface $entityManager,
     ) {
     }
 
@@ -25,6 +27,44 @@ class OrderStateService
         $this->assignStatus(
             $order,
             OrderTransitions::TO_CONFIRMED,
+        );
+    }
+
+    /**
+     * @throws \App\Domain\Order\Exceptions\OrderStatusException
+     */
+    public function assignCancel(Order $order): void
+    {
+        $this->assignStatus(
+            $order,
+            OrderTransitions::TO_CANCELLED,
+        );
+    }
+
+    public function canCancel(Order $order): bool
+    {
+        return $this->ordersStateMachine->can(
+            $order,
+            OrderTransitions::TO_CANCELLED,
+        );
+    }
+
+    /**
+     * @throws \App\Domain\Order\Exceptions\OrderStatusException
+     */
+    public function assignShip(Order $order): void
+    {
+        $this->assignStatus(
+            $order,
+            OrderTransitions::TO_SHIPPED,
+        );
+    }
+
+    public function canShip(Order $order): bool
+    {
+        return $this->ordersStateMachine->can(
+            $order,
+            OrderTransitions::TO_SHIPPED,
         );
     }
 
@@ -48,6 +88,10 @@ class OrderStateService
             $order,
             $transitionName,
         );
+        
+        $this->entityManager
+            ->flush()
+        ;
     }
 
     /**
