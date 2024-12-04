35485276537610a30358e947f4dfd6d68e0016f4
jmpala <juanmartinpalazzolo@icloud.com>
2024-12-04 13:06:22 +0100

    86c17e7d3
    - handle cancellation of an order

--- BE/src/Domain/Order/Controller/OrderAdminController.php
+++ BE/src/Domain/Order/Controller/OrderAdminController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Domain\Order\Controller;
 
 use App\Domain\Order\Service\OrderService;
+use App\Domain\Order\Service\OrderStateService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,6 +17,7 @@ class OrderAdminController extends AbstractController
 {
     public function __construct(
         private readonly OrderService $orderService,
+        private readonly OrderStateService $orderStateService,
     ) {
     }
 
@@ -46,13 +48,47 @@ class OrderAdminController extends AbstractController
         Request $request,
     ): Response {
         $order = $this->orderService
-            ->getOrderById($id);
+            ->getOrderById($id)
+        ;
 
         return $this->render(
             'admin/order/show_order.html.twig',
             [
                 'order' => $order,
             ],
+        );
+    }
+
+    /**
+     * @throws \Exception
+     */
+    #[Route('/admin/orders/{id}/status/cancel', name: 'admin_order_status_cancel')]
+    public function cancel(int $id): Response
+    {
+        $order = $this->orderService
+            ->getOrderById($id)
+        ;
+        $canCanel = $this->orderStateService
+            ->canCancel($order);
+
+        if ($canCanel) {
+            $this->orderStateService
+                ->assignCancel($order);
+
+            $this->addFlash(
+                'success',
+                'Order has been cancelled',
+            );
+        } else {
+            $this->addFlash(
+                'error',
+                'Order cannot be cancelled',
+            );
+        }
+
+        return $this->redirectToRoute(
+            'admin_order_show',
+            ['id' => $id],
         );
     }
 }
