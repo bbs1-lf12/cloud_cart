cf36337cec6fd08d3363e13f67e1d13f1324fd87
jmpala <juanmartinpalazzolo@icloud.com>
2024-12-04 13:05:54 +0100

    86c17e7d3
    - added twig functions to show buttons on status cancel & ship

---
+++ BE/src/Domain/Order/Twig/OrderExtension.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Order\Twig;
+
+use App\Domain\Order\Entity\Order;
+use App\Domain\Order\Service\OrderStateService;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class OrderExtension extends AbstractExtension
+{
+    public function __construct(
+        private readonly OrderStateService $orderStateService,
+    ) {
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction(
+                'canCancel',
+                [
+                    $this,
+                    'canCancel',
+                ],
+            ),
+            new TwigFunction(
+                'canShip',
+                [
+                    $this,
+                    'canShip',
+                ],
+            ),
+        ];
+    }
+
+    public function canCancel(Order $order): bool
+    {
+        return $this->orderStateService
+            ->canCancel($order)
+        ;
+    }
+
+    public function canShip(Order $order): bool
+    {
+        return $this->orderStateService
+            ->canShip($order)
+        ;
+    }
+}
