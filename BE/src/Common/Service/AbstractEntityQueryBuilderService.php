e67dfc4b9405a1ed04c4c1261882436082d0983c
jmpala <juanmartinpalazzolo@icloud.com>
2024-12-24 20:50:58 +0100

    - added AbstractEntity as default on ENTITY_FQN and also changed static for self

--- BE/src/Common/Service/AbstractEntityQueryBuilderService.php
+++ BE/src/Common/Service/AbstractEntityQueryBuilderService.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace App\Common\Service;
 
+use App\Common\Entity\AbstractEntity;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\QueryBuilder;
 
 abstract class AbstractEntityQueryBuilderService
 {
     protected const ALIAS = '';
-    protected const ENTITY_FQN = '';
+    protected const ENTITY_FQN = AbstractEntity::class;
 
     public function __construct(
         protected readonly EntityManagerInterface $entityManager
@@ -20,7 +21,7 @@ abstract class AbstractEntityQueryBuilderService
     protected function getEntityQueryBuilder(): QueryBuilder
     {
         return $this->entityManager
-            ->getRepository(static::ENTITY_FQN)
+            ->getRepository(self::ENTITY_FQN)
             ->createQueryBuilder(static::ALIAS)
         ;
     }
