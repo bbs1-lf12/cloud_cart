46c9d04a294cd940584c534ecda2554b34f1c30d
jmpala <juanmartinpalazzolo@icloud.com>
2024-12-26 18:35:52 +0100

    - listing products from BE

--- FE/src/components/Products.jsx
+++ FE/src/components/Products.jsx
@@ -1,29 +1,28 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import ProductItem from "./ProductItem";
-import BEConnection from "../services/BEConnection";
+import BEConnectionService from "../services/BEConnectionService.js";
 
 export default function Products() {
-  const [loadedMeals, setLoadedMeals] = useState([]); //am anfang ist das Array noch leer da die Produkte noch nicht geladen werden
+  const [loadedProducts, setLoadedProducts] = useState([]); //am anfang ist das Array noch leer da die Produkte noch nicht geladen werden
 
   useEffect(() => {
-    async function fetchMeals() {
+    async function fetchProducts() {
       try {
-        const meals = await new BEConnection().getAllArticles();
-        setLoadedMeals(meals);
+        const products = await BEConnectionService.getInstance().getAllArticles();
+        setLoadedProducts(products);
       } catch (error) {
         console.error(error); // Füge Fehlerprotokollierung hinzu
       }
     }
 
-    fetchMeals();  // fetchMeals nur innerhalb von useEffect aufrufen
+    fetchProducts();  // fetchMeals nur innerhalb von useEffect aufrufen
   }, []);
-
 
 
   return (
     <ul id="products">
-      {loadedMeals.map((meal) => (
-        <ProductItem key={ProductItem.id} product={meal} />
+      {loadedProducts.map((p) => (
+        <ProductItem key={p.id} product={p}/>
       ))}
     </ul>
   );
@@ -31,30 +30,30 @@ export default function Products() {
 
 /**========================================================================
  * *    	                           INFO
-  //? Hooks sind spezielle Funktionen idiesem Fall von React selbst
+ //? Hooks sind spezielle Funktionen idiesem Fall von React selbst
 
  *   useState:
-		 (Zustandsvariable) ist ein Speicherplatz, der Daten enthält, die sich
-		 ändern können. Und React ermöglicht es diese Daten automatisch zu verwalten und Darzustellen.
+ (Zustandsvariable) ist ein Speicherplatz, der Daten enthält, die sich
+ ändern können. Und React ermöglicht es diese Daten automatisch zu verwalten und Darzustellen.
 
  *   loadedMeals: Dies ist die Variable, die den aktuellen Wert des "State" speichert. In
- *	 setLoadedMeals: ist die Funktion, mit der die var(loadedMeals) geändert werden kann.
+ *   setLoadedMeals: ist die Funktion, mit der die var(loadedMeals) geändert werden kann.
 
- *	 Beispielhafter Ablauf:
-    1. Beim ersten Rendern der Komponente ist loadedMeals leer ([]).
-    2. Nachdem die Daten geladen wurden (z.B. in fetchMeals()), rufst du setLoadedMeals(meals) auf.
-    3. Dadurch ändert sich loadedMeals und speichert die vom Server abgerufenen Mahlzeiten (z.B. eine Liste
-			 von Objekten wie { id: '1', name: 'Pizza', price: 10.99 }).
-    4. Da sich der "State" geändert hat, wird die Komponente neu gerendert und zeigt die neuen Daten an.
+ *   Beispielhafter Ablauf:
+ 1. Beim ersten Rendern der Komponente ist loadedMeals leer ([]).
+ 2. Nachdem die Daten geladen wurden (z.B. in fetchMeals()), rufst du setLoadedMeals(meals) auf.
+ 3. Dadurch ändert sich loadedMeals und speichert die vom Server abgerufenen Mahlzeiten (z.B. eine Liste
+ von Objekten wie { id: '1', name: 'Pizza', price: 10.99 }).
+ 4. Da sich der "State" geändert hat, wird die Komponente neu gerendert und zeigt die neuen Daten an.
 
  *   useEffect:
-		 - ist ein "side effect" (das Abrufen von Daten von einem Server)
-		 - stellt sicher, dass der Code für das Laden der Daten nur einmal ausgeführt wird, nämlich beim ersten Rendern der Komponente.
+ - ist ein "side effect" (das Abrufen von Daten von einem Server)
+ - stellt sicher, dass der Code für das Laden der Daten nur einmal ausgeführt wird, nämlich beim ersten Rendern der Komponente.
 
-*		 async:
-     - ist eine spezielle Funktion, die etwas tut, was länger dauert(das laden von Daten)
-     - dadurch muss das Programm nicht angehalten werden.
-     - mit await wird auf das Laden der Daten gewartet.
-     -
+ *     async:
+ - ist eine spezielle Funktion, die etwas tut, was länger dauert(das laden von Daten)
+ - dadurch muss das Programm nicht angehalten werden.
+ - mit await wird auf das Laden der Daten gewartet.
+ -
 
  *========================================================================**/
