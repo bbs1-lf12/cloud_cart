1d5fe09b18c2ace8092174888be6dd198ae9913b
jmpala <juanmartinpalazzolo@icloud.com>
2024-12-26 10:48:01 +0100

    - save darkmode state into localstorage

--- BE/assets/js/backoffice/darkModeToogle.js
+++ BE/assets/js/backoffice/darkModeToogle.js
@@ -2,16 +2,37 @@
   document.addEventListener('DOMContentLoaded', () => {
     const body = document.querySelector('html');
 
+    const darkModeKey = 'darkMode';
+    const isDarkMode = localStorage.getItem(darkModeKey);
+
+    function setLightMode() {
+      localStorage.setItem(darkModeKey, 'false');
+      icon.classList.remove('moon-icon');
+      icon.classList.add('sun-icon');
+      body.classList.remove('dark');
+    }
+
+    function setDarkMode() {
+      localStorage.setItem(darkModeKey, 'true');
+      icon.classList.remove('sun-icon');
+      icon.classList.add('moon-icon');
+      body.classList.add('dark');
+    }
+
     function toggleMode() {
-      if (icon.classList.contains('sun-icon')) {
-        icon.classList.remove('sun-icon');
-        icon.classList.add('moon-icon');
-        body.classList.add('dark');
-      } else if (icon.classList.contains('moon-icon')) {
-        icon.classList.remove('moon-icon');
-        icon.classList.add('sun-icon');
-        body.classList.remove('dark');
+      if (body.classList.contains('dark')) {
+        setLightMode();
+      } else {
+        setDarkMode();
       }
+    }
+
+    if (isDarkMode !== 'true') {
+      localStorage.setItem(darkModeKey, 'false');
+      body.classList.remove('dark');
+    } else {
+      localStorage.setItem(darkModeKey, 'true');
+      body.classList.add('dark');
     }
 
     const icon = document.getElementById('dark-mode');
