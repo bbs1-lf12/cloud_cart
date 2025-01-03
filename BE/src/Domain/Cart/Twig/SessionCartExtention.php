3da36a0b031afbad2fd0c4b50992e6c843868e8c
jmpala <juanmartinpalazzolo@icloud.com>
2025-01-03 07:00:17 +0100

    86c1fyc9v
    - refactor cart to work with the session and the user date, when logged-in

--- BE/src/Domain/Cart/Twig/SessionCartExtention.php
+++ BE/src/Domain/Cart/Twig/SessionCartExtention.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace App\Domain\Cart\Twig;
 
 use App\Domain\Article\Entity\Article;
+use App\Domain\Cart\Entity\Cart;
+use App\Domain\Cart\Service\CartFOService;
 use App\Domain\Cart\Service\CartSessionService;
+use App\Domain\User\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -14,7 +18,9 @@ class SessionCartExtention extends AbstractExtension
 {
     public function __construct(
         private readonly CartSessionService $cartSessionService,
+        private readonly CartFOService $cartFOService,
         private readonly EntityManagerInterface $entityManager,
+        private readonly Security $security,
     ) {
     }
 
@@ -23,17 +29,17 @@ class SessionCartExtention extends AbstractExtension
         return [
             new TwigFunction(
                 'getSessionCart',
-                [
-                    $this,
-                    'getSessionCart',
-                ],
+                $this->getSessionCart(...),
+            ),
+            new TwigFunction(
+                'getUserCart',
+                $this->getUserCart(...),
             ),
         ];
     }
 
     public function getSessionCart(): array
     {
-        // TODO-JMP: do not execute if the user is logged in!!!
         $cart = $this->cartSessionService
             ->getSessionCart()
         ;
@@ -61,5 +67,14 @@ class SessionCartExtention extends AbstractExtension
             $cart,
             $articles,
         );
+    }
+
+    public function getUserCart(): Cart
+    {
+        /** @var User $user */
+        $user = $this->security
+            ->getUser();
+        return $this->cartFOService
+            ->getCart($user);
     }
 }
