110223487a898fb658f89f1cc00c994885d9adaf
Juan M. Palazzolo <j.palazzolo@local-brand-x.com>
2025-01-09 11:35:21 +0100

    - added gd lib - uploaded images resized when upload

--- BE/src/Domain/Article/Service/ImageService.php
+++ BE/src/Domain/Article/Service/ImageService.php
@@ -11,6 +11,7 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 class ImageService
 {
     private string $targetDirectory;
+
     public function __construct(
         private readonly SluggerInterface $slugger,
     ) {
@@ -29,8 +30,13 @@ class ImageService
         $fileName = $safeFilename . '-' . uniqid() . '.' . $file->guessExtension();
 
         try {
+            $this->resizeImage(
+                $file,
+                $file->getFilename(),
+            );
+
             $file->move(
-                $this->getTargetDirectory(),
+                $this->targetDirectory,
                 $fileName,
             );
         } catch (FileException $e) {
@@ -42,8 +48,105 @@ class ImageService
         return $fileName;
     }
 
-    public function getTargetDirectory(): string
-    {
-        return $this->targetDirectory;
+    public function resizeImage(
+        UploadedFile $file,
+        string $fileName,
+    ): void {
+        /** @var ?\GdImage $image */
+        $uploadedImg = $this->getImage(
+            $file,
+            $fileName,
+        );
+        if (!$uploadedImg) {
+            return;
+        }
+
+        $uploadedWidth = imagesx($uploadedImg);
+        $uploadedHeight = imagesy($uploadedImg);
+        $greaterSide = max(
+            $uploadedWidth,
+            $uploadedHeight,
+        );
+
+        $image = imagecreatetruecolor(
+            $greaterSide,
+            $greaterSide,
+        );
+        $white = imagecolorallocate(
+            $image,
+            255,
+            255,
+            255,
+        );
+        imagefill(
+            $image,
+            0,
+            0,
+            $white,
+        );
+
+        $x = (int) (($greaterSide - $uploadedWidth) / 2);
+        $y = (int) (($greaterSide - $uploadedHeight) / 2);
+
+        imagecopy(
+            $image,
+            $uploadedImg,
+            $x,
+            $y,
+            0,
+            0,
+            $uploadedWidth,
+            $uploadedHeight,
+        );
+
+        $this->saveImage(
+            $file,
+            $fileName,
+            $image,
+        );
+    }
+
+    private function getImage(
+        UploadedFile $file,
+        string $fileName,
+    ): ?\GdImage {
+        $extension = $file->guessExtension();
+        if (
+            $extension === "jpg"
+            || $extension === "jpeg"
+        ) {
+            return imagecreatefromjpeg($file->getPath() . '/' . $fileName);
+        }
+        if (
+            $extension === "png"
+        ) {
+            return imagecreatefromjpeg($file->getPath() . '/' . $fileName);
+        }
+        return null;
+    }
+
+    private function saveImage(
+        UploadedFile $file,
+        string $fileName,
+        \GdImage $image,
+    ): void {
+        $extension = $file->guessExtension();
+        if (
+            $extension === "jpg"
+            || $extension === "jpeg"
+        ) {
+            imagejpeg(
+                $image,
+                $file->getPath() . '/' . $fileName,
+            );
+        }
+        if (
+            $extension === "png"
+        ) {
+            imagepng(
+                $image,
+                $file->getPath() . '/' . $fileName,
+            );
+        }
     }
 }
