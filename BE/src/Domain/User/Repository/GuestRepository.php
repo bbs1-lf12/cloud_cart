3d5e7396109d5ee7efd358d6dd2d133b4a8c3697
jmpala <juanmartinpalazzolo@icloud.com>
2025-01-16 10:41:57 +0100

    86c1n717n
    - added Guest repository class

---
+++ BE/src/Domain/User/Repository/GuestRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\User\Repository;
+
+use App\Domain\User\Entity\Guest;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Guest>
+ */
+class GuestRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Guest::class);
+    }
+}
